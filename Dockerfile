07e1f9f59f8ece38741f655a1c5e47e0824755e8
machiza <machizaarmando@gmail.com>
2022-03-28 12:33:02 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:16
+WORKDIR /app
+COPY package.json .
+
+ARG NODE_ENV
+RUN if [ "$NODE_ENV" = "development" ]; \
+          then npm install; \
+          else npm install --only=production; \
+          fi
+
+RUN npm install
+COPY . .
+EXPOSE 3000
+CMD ["npm", "run", "dev"]
---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const redis = require('redis');
+
+const RedisStore = require('connect-redis')(session);
+
+const { 
+  MONGO_USER, 
+  MONGO_PASSWORD, 
+  MONGO_IP, 
+  MONGO_PORT,
+  REDIS_URL,
+	REDIS_PORT,
+	SESSION_SECRET,
+} = require('./config/config');
+
+console.log("REDIS_URL: ", REDIS_URL)
+
+let redisClient = redis.createClient({
+  legacyMode: true,
+  socket: {
+    host: REDIS_URL,
+    port: REDIS_PORT,
+  }
+});
+redisClient.connect().catch(console.error)
+
+const postRouter = require('./routes/postRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('successfully connected to DB'))
+  .catch((e) => {
+    console.log(e);
+    setTimeout(connectWithRetry, 5000);
+  });
+}
+
+connectWithRetry();
+
+app.use(session({
+  store: new RedisStore({client: redisClient}),
+  secret: SESSION_SECRET,
+  cookie: {
+    secure: false,
+    resave: false,
+    saveUnitialized: false,
+    httpOnly: true,
+    maxAge: 60000
+  }
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h2>Hi There</h2>');
+});
+
+app.use('/api/v1/posts', postRouter);
+app.use("/api/v1/users", userRouter);
+const port = process.env.PORT || 3000;
+
+app.listen(port, async () => { 
+  // await redisClient.connect();
+  console.log(`🚀 listening on port ${port}`);
+});
